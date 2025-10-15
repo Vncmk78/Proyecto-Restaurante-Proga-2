3a05059cfb0df70f983a3b3e605b30af28910695
bliempi2025 <bliempi2025@alu.uct.cl>
2025-10-15 14:29:09 -0300

    fix: metodos agregar/eliminar acepta mayusculas

--- ev2_progra2/Stock.py
+++ ev2_progra2/Stock.py
@@ -5,18 +5,22 @@ class Stock:
         self.lista_ingredientes = []
 
     def agregar_ingrediente(self, ingrediente):
+        nombre_ing = ingrediente.nombre.lower() # convertimos a minusculas para evitar errores
         for ing in self.lista_ingredientes:
-            if ing.nombre == ingrediente.nombre and ing.unidad == ingrediente.unidad:
-                ing.cantidad += ingrediente.cantidad
+            nombre_ing_existente = ing.nombre.lower() 
+            if nombre_ing_existente == nombre_ing: # si el ingrediente ya existe, sumamos la cantidad
+                ing.cantidad += float(ingrediente.cantidad) # convertimos a float para evitar errores
                 return
         self.lista_ingredientes.append(ingrediente)
 
     def eliminar_ingrediente(self, nombre_ingrediente):
+        nombre_ingrediente = nombre_ingrediente.lower()
         for ing in self.lista_ingredientes:
-            if ing.nombre == nombre_ingrediente:
+            if ing.nombre.lower() == nombre_ingrediente:
                 self.lista_ingredientes.remove(ing)
                 return True
-
+        return False
+    
     def verificar_stock(self):
         ingredientes_disponibles = []
         for ing in self.lista_ingredientes:
