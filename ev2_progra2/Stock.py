c10c84c82b641ecbac264592a13753895387e435
Vicente <bicho2.2.2005@gmail.com>
2025-10-17 04:21:52 -0300

    definitivo

--- ev2_progra2/Stock.py
+++ ev2_progra2/Stock.py
@@ -5,17 +5,41 @@ class Stock:
         self.lista_ingredientes = []
 
     def agregar_ingrediente(self, ingrediente):
-        pass
+        nombre_ing = ingrediente.nombre.lower() # convertimos a minusculas para evitar errores
+        for ing in self.lista_ingredientes:
+            nombre_ing_existente = ing.nombre.lower() 
+            if nombre_ing_existente == nombre_ing: # si el ingrediente ya existe, sumamos la cantidad
+                ing.cantidad += float(ingrediente.cantidad) # convertimos a float para evitar errores
+                return
+        self.lista_ingredientes.append(ingrediente)
 
     def eliminar_ingrediente(self, nombre_ingrediente):
-        pass    
-
+        nombre_ingrediente = nombre_ingrediente.lower()
+        for ing in self.lista_ingredientes:
+            if ing.nombre.lower() == nombre_ingrediente:
+                self.lista_ingredientes.remove(ing)
+                return True
+        return False
+    
     def verificar_stock(self):
-        pass
+        ingredientes_disponibles = []
+        for ing in self.lista_ingredientes:
+            if ing.cantidad > 0:
+                ingredientes_disponibles.append(ing)
+        return ingredientes_disponibles
 
     def actualizar_stock(self, nombre_ingrediente, nueva_cantidad):
-        pass
+        nombre_ingrediente = nombre_ingrediente.lower()
+        for ing in self.lista_ingredientes:
+            if ing.nombre.lower() == nombre_ingrediente:
+                ing.cantidad = float(nueva_cantidad) # convertimos a float para evitar errores
+                return True
+        return False
 
     def obtener_elementos_menu(self):
-        pass
+        elementos_menu = [] # creamos lista para guardar los nombres de los ingredientes disponibles
+        for ing in self.lista_ingredientes: 
+            if ing.cantidad > 0: # solo agregamos los ingredientes con cantidad > 0
+                elementos_menu.append(ing.nombre)
 
+        return elementos_menu
